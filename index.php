04919c3d8ecf2d413479696b59b2bea20cc4e5d7
Edgar Castro <edgar.castro.vm@gmail.com>
2019-07-18 11:30:49 -0500

    agregando direccion y saltos de linea

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 
-echo "linea 1 nombre";
-echo "linea 2 apellido";
-echo "linea 3 cedula";
-echo "linea 4 edad";
-echo "linea 5 email";
+echo "linea 1 nombre <br>";
+echo "linea 2 apellido<br>";
+echo "linea 3 cedula<br>";
+echo "linea 4 edad<br>";
+echo "linea 5 email<br>";
+echo "linea 5 direccion<br>";
